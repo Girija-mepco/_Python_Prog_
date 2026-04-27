3be6eecc676055a1dfb771ddfc3702b0ef0122c2
Girija-mepco <gandhigovindaraj_bec28@mepcoeng.ac.in>
2026-04-27 11:40:47 +0530

    Add files via upload

---
+++ dict2.py
@@ -0,0 +1,13 @@
+# Read the first integer a
+a = int(input())
+# Read the second integer b
+b = int(input())
+
+# First line: Integer division (//)
+print(a // b)
+
+# Second line: Modulo operator (%)
+print(a % b)
+
+# Third line: divmod(a, b)
+print(divmod(a, b))
